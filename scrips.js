9598c4f0583aa9fe4c333dfa313400ae1b7930e4
Alex <alexzlkl@yandex.ru>
2020-11-04 21:45:20 +0300

    feat: cliens slider

--- scrips.js
+++ scrips.js
@@ -70,3 +70,29 @@ logo.forEach((element) => {
     // background.style.backgroundColor = "#ffffff";
   };
 });
+
+
+
+////slider
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const itemsList = document.querySelector("#items");
+
+const loop = (direction, e) => {
+  e.preventDefault();
+
+  if (direction === "right") {
+    itemsList.appendChild(itemsList.firstElementChild);
+  } else {
+    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
+  }
+};
+
+right.addEventListener("click", (e) => {
+  loop("right", e);
+});
+
+left.addEventListener("click", (e) => {
+  loop("left", e);
+});

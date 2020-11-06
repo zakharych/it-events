1101efc98f368d0dc76f9de94d28976523ecd36f
zakharych <alexzlkl@yandex.ru>
2020-11-06 21:38:47 +0300

    fix: bg & section width

--- scrips.js
+++ scrips.js
@@ -1,58 +1,53 @@
 const logo = document.querySelectorAll(".conf__logo"),
-  background = document.querySelector(".conf-section");
+  background = document.querySelector(".conf-section"),
+  item = document.querySelectorAll(".conf__item");
 
-
-logo.forEach((element) => {
+item.forEach((element) => {
   element.onmouseover = function (event) {
     let target = event.target;
-    // console.log(element.lastElementChild);
-    // console.log(logo[0].lastElementChild);
-    // console.log(target);
-    // console.log(logo);
+    console.log(target);
 
-    target.style.opacity = "1";
-    if (element.lastElementChild === logo[0].lastElementChild) {
-      background.style.backgroundColor = "#f24944";
-      element.style.opacity = "1";
-      logo.forEach((element) => {
-        if (element.lastElementChild !== logo[0].lastElementChild) {
-          element.style.opacity = "0.5";
-        }
-      });
-    } else if (element.lastElementChild === logo[1].lastElementChild) {
-      background.style.backgroundColor = "#316696";
-      logo.forEach((element) => {
-        element.style.opacity = "1";
-        if (element.lastElementChild !== logo[1].lastElementChild) {
-          element.style.opacity = "0.5";
-        }
-      });
-    } else if (element.lastElementChild === logo[2].lastElementChild) {
-      background.style.backgroundColor = "#36a9e1";
-      element.style.opacity = "1";
-      logo.forEach((element) => {
-        if (element.lastElementChild !== logo[2].lastElementChild) {
-          element.style.opacity = "0.5";
-        }
-      });
-    } else if (element.lastElementChild === logo[3].lastElementChild) {
-      background.style.backgroundColor = "#2d396b";
-      element.style.opacity = "1";
-      logo.forEach((element) => {
-        if (element.lastElementChild !== logo[3].lastElementChild) {
-          element.style.opacity = "0.5";
-        }
-      });
-    }
+    
+    //   target.style.opacity = "1";
+    //   if (element.lastElementChild === logo[0].lastElementChild) {
+    //     background.style.backgroundColor = "#f24944";
+    //     element.style.opacity = "1";
+    //     logo.forEach((element) => {
+    //       if (element.lastElementChild !== logo[0].lastElementChild) {
+    //         element.style.opacity = "0.5";
+    //       }
+    //     });
+    //   } else if (element.lastElementChild === logo[1].lastElementChild) {
+    //     background.style.backgroundColor = "#316696";
+    //     logo.forEach((element) => {
+    //       element.style.opacity = "1";
+    //       if (element.lastElementChild !== logo[1].lastElementChild) {
+    //         element.style.opacity = "0.5";
+    //       }
+    //     });
+    //   } else if (element.lastElementChild === logo[2].lastElementChild) {
+    //     background.style.backgroundColor = "#36a9e1";
+    //     element.style.opacity = "1";
+    //     logo.forEach((element) => {
+    //       if (element.lastElementChild !== logo[2].lastElementChild) {
+    //         element.style.opacity = "0.5";
+    //       }
+    //     });
+    //   } else if (element.lastElementChild === logo[3].lastElementChild) {
+    //     background.style.backgroundColor = "#2d396b";
+    //     element.style.opacity = "1";
+    //     logo.forEach((element) => {
+    //       if (element.lastElementChild !== logo[3].lastElementChild) {
+    //         element.style.opacity = "0.5";
+    //       }
+    //     });
+    //   }
   };
 });
 
 logo.forEach((element) => {
   element.onmouseout = function (event) {
     let target = event.target;
-    // target.style.opacity = "0.5";
-    // console.log(target);
-    // background.style.backgroundColor = "#ffffff";
   };
 });
 
@@ -94,9 +89,8 @@ const next = document.querySelector("#next");
 const slider = document.querySelector("#hero__slider");
 
 const slide = (direction) => {
-  
-  next.classList.toggle('hero__slider--active');
-  prev.classList.toggle('hero__slider--active');
+  next.classList.toggle("hero__slider--active");
+  prev.classList.toggle("hero__slider--active");
   if (direction === "right") {
     slider.appendChild(slider.firstElementChild);
   } else {
@@ -115,7 +109,6 @@ nextSlide();
 prev.addEventListener("click", (e) => {
   e.preventDefault();
   slide("right");
-
 });
 
 next.addEventListener("click", (e) => {

ab278e5bc70bda6ea576ebe3e70d3e30f297d3ae
zakharych <alexzlkl@yandex.ru>
2020-11-17 00:58:18 +0300

    fix: btn slider

--- sliderSetup.js
+++ sliderSetup.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
     appendArrows: ".arrows",
     appendDots: ".arrows",
     prevArrow:
-      '<div id="prev" type="button" class="btn__hero-slider-l btn__hero-slider-l--disabled  btn-juliet"><img src="https://it-events.com/system/attachments/files/000/002/012/original/left_arrow.svg" alt="<" class="arrow__img"></div>',
+      '<div id="prev" class="btn__hero-slider-l btn__hero-slider-l--disabled  btn-juliet"><img src="https://it-events.com/system/attachments/files/000/002/012/original/left_arrow.svg" alt="<" class="arrow__img"></div>',
     nextArrow:
-      '<div id="next" type="button" class="btn__hero-slider-r btn__hero-slider-r--disabled btn-juliet"><img src="https://it-events.com/system/attachments/files/000/002/013/original/right_arrow.svg" alt=">" class="arrow__img"></div>',
+      '<div id="next" class="btn__hero-slider-r btn__hero-slider-r--disabled btn-juliet"><img src="https://it-events.com/system/attachments/files/000/002/013/original/right_arrow.svg" alt=">" class="arrow__img"></div>',
     asNavFor: ".sliderBg",
   });
   $(".sliderBg").slick({
@@ -26,8 +26,8 @@ $(document).ready(function () {
     dots: true,
     adaptiveHeight: true,
     prevArrow:
-      '<div id="prev" type="button" class=" reviews__arrow reviews__arrow-l"><img src="https://it-events.com/system/attachments/files/000/002/012/original/left_arrow.svg" alt="<" class="arrow__img"></div>',
+      '<div id="prev" class=" reviews__arrow reviews__arrow-l"><img src="https://it-events.com/system/attachments/files/000/002/012/original/left_arrow.svg" alt="<" class="arrow__img"></div>',
     nextArrow:
-      '<div id="next" type="button" class=" reviews__arrow reviews__arrow-r"><img src="https://it-events.com/system/attachments/files/000/002/013/original/right_arrow.svg" alt=">" class="arrow__img"></div>',
+      '<div id="next" class=" reviews__arrow reviews__arrow-r"><img src="https://it-events.com/system/attachments/files/000/002/013/original/right_arrow.svg" alt=">" class="arrow__img"></div>',
   });
 });
